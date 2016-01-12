147a012a313729dd4ed57276694b64fab3a0bf6d
Fran Varney <fran@modulus.io>
2016-01-12 11:43:45 -0500

    Add start of App container

---
+++ client/containers/App.js
@@ -0,0 +1,13 @@
+import React, {createClass} from 'react'
+
+export default createClass({
+  displayName: 'AppContainer',
+
+  render() {
+    return (
+      <div>
+        Test
+      </div>
+    )
+  }
+})
