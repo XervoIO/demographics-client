037b635e697afdc240b98434d62d520b7218f370
Fran Varney <fran@modulus.io>
2016-01-15 09:34:55 -0500

    Add thunk and api middleware

--- client/store/configureStore.dev.js
+++ client/store/configureStore.dev.js
@@ -1,15 +1,15 @@
 import {createHistory} from 'history'
 import {applyMiddleware, createStore, compose} from 'redux'
+import {apiMiddleware} from 'redux-api-middleware'
 import createLogger from 'redux-logger'
 import {reduxReactRouter} from 'redux-router'
-// import Thunk from 'redux-thunk'
+import Thunk from 'redux-thunk'
 
-// import api from '../middleware/api'
 import DevTools from '../containers/DevTools'
 import RootReducer from '../reducers'
 
 const finalCreateStore = compose(
-  // applyMiddleware(Thunk, api),
+  applyMiddleware(Thunk, apiMiddleware),
   reduxReactRouter({ createHistory }),
   applyMiddleware(createLogger()),
   DevTools.instrument()
--- client/store/configureStore.prod.js
+++ client/store/configureStore.prod.js
@@ -1,13 +1,13 @@
 import {createHistory} from 'history'
 import {applyMiddleware, createStore, compose} from 'redux'
+import {apiMiddleware} from 'redux-api-middleware'
 import {reduxReactRouter} from 'redux-router'
-// import Thunk from 'redux-thunk'
+import Thunk from 'redux-thunk'
 
-// import api from '../middleware/api'
 import RootReducer from '../reducers'
 
 const finalCreateStore = compose(
-  // applyMiddleware(Thunk, api),
+  applyMiddleware(Thunk, apiMiddleware),
   reduxReactRouter({ createHistory })
 )(createStore)
 
