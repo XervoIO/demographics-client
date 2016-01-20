78ce9e012987361d0836d251b7ec1ba567549ea3
Fran Varney <fran@modulus.io>
2016-01-20 14:26:15 -0500

    Add header

---
+++ client/components/Layout/Header.js
@@ -0,0 +1,14 @@
+import React, {Component, PropTypes} from 'react'
+
+export default class Header extends Component {
+  render() {
+    return (
+      <header>
+        <h1>Demographics</h1>
+      </header>
+    )
+  }
+}
+
+Header.displayName = 'Header'
+
--- client/layouts/Default.js
+++ client/layouts/Default.js
@@ -1,10 +1,12 @@
 import React, {Component, PropTypes} from 'react'
 
+import Header from '../components/Layout/Header'
+
 export default class DefaultLayout extends Component {
   render() {
     return (
       <div>
-        Layout
+        <Header />
         {this.props.children}
       </div>
     )
